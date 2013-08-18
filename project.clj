97e29966633be2e56d4cfcce8f21de2e908d4c91
Sean Brewer <seabre986@gmail.com>
2013-08-17 23:47:21 -0400

    Add leiningen min version to project.clj, mainly for heroku.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject htmltohiccup "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :min-lein-version "2.0.0"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
