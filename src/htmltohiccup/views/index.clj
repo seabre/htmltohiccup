996b828d2f969eb14a48bc0ade911b3b1efcaf2c
Sean Brewer <seabre986@gmail.com>
2013-08-18 00:01:12 -0400

    Update google analytics.

--- src/htmltohiccup/views/index.clj
+++ src/htmltohiccup/views/index.clj
@@ -47,4 +47,4 @@
      [:script {:src "js/vendor/ace/ace.js" :type "text/javascript" :charset "utf-8"}]
      [:script {:src "js/main.js"}]
      [:script
-      "\n            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];\n            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];\n            g.src='//www.google-analytics.com/ga.js';\n            s.parentNode.insertBefore(g,s)}(document,'script'));\n        "]]]))
+       "\n  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\n  ga('create', 'UA-5393331-3', 'herokuapp.com');\n  ga('send', 'pageview');\n\n"]]]))
